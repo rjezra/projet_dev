3a654b5227aa9eedc2e171cf7080c619caac9f0f
rjezra <jahoely@gmail.com>
2024-05-10 14:45:39 +0300

    affichage utilisateur

--- index.php
+++ index.php
@@ -4,17 +4,17 @@ require('./elements/header.php');
 require_once('./function/function.php');
 
 $results = null;
-if(isset($_POST['afficher']) && isset($_POST['id_afficher'])){
+if (isset($_POST['afficher']) && isset($_POST['id_afficher'])) {
     $id = $_POST['id_afficher'];
     $results = informationPersonnelleUtilisateur($connexion, $id);
 }
-if(isset($_POST['edit']) && isset($_POST['id_edit'])){
-  $id = $_POST['id_edit'];
-  header("Location: pages/update.php?id=$id");
-  exit;
+if (isset($_POST['edit']) && isset($_POST['id_edit'])) {
+    $id = $_POST['id_edit'];
+    header("Location: pages/update.php?id=$id");
+    exit;
 }
 
-if(isset($_POST['supprimer'])){
+if (isset($_POST['supprimer'])) {
     supprimerUser($connexion, $_POST['id_supprimer']);
 }
 
@@ -22,7 +22,9 @@ $utilisateurs = listUtilisateur($connexion);
 ?>
 <div class="row">
     <div class="col-7">
-        <h1>Liste de utilisateurs</h1>
+        <?php foreach($utilisateurs as $utili):?>
+        <h1>Liste<?=count($utili) > 2 ? "s" : "" ?> de utilisateurs</h1>
+        <?php endforeach ?>
         <table class="table table-hover">
             <thead>
                 <tr>
@@ -46,7 +48,7 @@ $utilisateurs = listUtilisateur($connexion);
                                     <input type="hidden" name="id_afficher" value="<?= $utilisateur['id'] ?>">
                                     <button type="submit" name="supprimer"><i class="bi bi-trash3" alt="suppression"></i></button>
                                     <input type="hidden" name="id_edit" value="<?= $utilisateur['id'] ?>">
-                                    <button type="submit" name="edit"><i class="bi bi-pencil-square"></i></button>  
+                                    <button type="submit" name="edit"><i class="bi bi-pencil-square"></i></button>
                                 </form>
                             </div>
                         </td>
@@ -57,13 +59,33 @@ $utilisateurs = listUtilisateur($connexion);
     </div>
     <div class="col-5">
         <h1>Information personnel</h1>
-        <?php 
-            if($results != null) {
-                echo "<p>Nom : " . $results['nom'] . "</p>";
-                echo "<p>Prénom : " . $results['prenom'] . "</p>";
-            } else {
-                echo "Aucune information disponible.";
-            }
+        <?php if ($results != null) { ?>
+            <div class="img">
+                <img src="./assests/img/<?= $results['photo'] ?>" alt="" class="profil">
+            </div>
+            <div class="info">
+                <div class="valeur">
+                    <h2>Nom: </h2>
+                    <h2><?= $results['nom'] ?></h2>
+                </div>
+                <div class="valeur">
+                    <h2>Prenom: </h2>
+                    <h2><?= $results['prenom'] ?></h2>
+                </div>
+                <div class="valeur">
+                    <h2>Adresse: </h2>
+                    <h2><?= $results['adresse'] ?></h2>
+                </div>
+                <div class="valeur">
+                    <h2>Email: </h2>
+                    <h2><?= $results['mail'] ?></h2>
+                </div>
+
+            </div>
+        <?php
+        } else {
+            echo "Aucune information disponible.";
+        }
         ?>
     </div>
 </div>
